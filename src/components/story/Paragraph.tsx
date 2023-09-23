7094698697e17d448220b252625cc2ade2be7927
blcklamb <whenucan35@gmail.com>
2023-09-24 00:33:48 +0900

    style: add textAlign props for paragraph

--- src/components/story/Paragraph.tsx
+++ src/components/story/Paragraph.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import { styled } from "styled-components";
 import { COLORS } from "styles/color";
 
-interface ParagraphPrpos {
+type TAlign = "left" | "center" | "right";
+interface ParagraphProps {
   children: React.ReactNode;
+  align?: TAlign;
 }
-const Paragraph = ({ children }: ParagraphPrpos) => {
-  return <StyledP>{children}</StyledP>;
+/**
+ * 
+ * @example
+ * <Paragraph align="center">
+    {`지구 사람들을 바라보니
+    걱정이 너무 많은 것 같아오!`}
+  </Paragraph>
+ */
+const Paragraph = ({ children, align = "left" }: ParagraphProps) => {
+  return <StyledP align={align}>{children}</StyledP>;
 };
 
 export default Paragraph;
 
-const StyledP = styled.p`
+const StyledP = styled.p<{ align: TAlign }>`
   color: ${COLORS.WHITE};
   white-space: pre-line;
   font-size: 3.6em;
+  line-height: normal;
+
+  text-align: ${(props) => props.align};
 `;

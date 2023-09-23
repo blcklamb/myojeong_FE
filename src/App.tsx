e9a8060d0544c0f41263834e06c134223ec45313
blcklamb <whenucan35@gmail.com>
2023-09-23 22:16:18 +0900

    add: Icon component - app global iconLoader - icon 'forward'

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Dalnara from "./layouts/Dalnara";
 import Story from "./layouts/Story";
 import GlobalStyle from "styles/GlobalStyle";
+import { IconLoader } from "components/IconLoader";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <IconLoader />
       <Routes>
         <Route path="/story" Component={Story} />
         <Route path="/dalnara" Component={Dalnara} />
---
+++ src/components/IconLoader.tsx
@@ -0,0 +1,21 @@
+export const IconLoader = () => (
+  <svg
+    id="sprite-symbols"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/symbol"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    style={{ display: "none" }}
+  >
+    <defs id="defs">
+      <symbol width="24" height="24" fill="none" id="forward">
+        <path
+          stroke="#7C644C"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1.5"
+          d="M10.137 15.42c.148 1.72.363 2.58.363 2.58s2-.5 5.5-2.5 4.5-3.5 4.5-3.5-1-1.5-4.5-3.5S10.5 6 10.5 6s-.216.866-.364 2.597m.001 6.824A40.021 40.021 0 0 1 10 12c0-1.369.059-2.503.136-3.403m.001 6.824C8.392 16.43 6.477 17.506 4.5 18c0 0-.5-2-.5-6s.5-6 .5-6c1.903.476 4.054 1.47 5.636 2.597"
+        />
+      </symbol>
+    </defs>
+  </svg>
+);

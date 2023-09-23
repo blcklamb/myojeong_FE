61aabff9368834c8febdaa79ec71f133af719bf9
blcklamb <whenucan35@gmail.com>
2023-09-24 00:46:58 +0900

    add: layout/NameTo

--- src/layouts/Story.tsx
+++ src/layouts/Story.tsx
@@ -1,5 +1,6 @@
 import Intro from "components/story/Intro";
 import NameFrom from "components/story/NameFrom";
+import NameTo from "components/story/NameTo";
 import Turn from "components/story/Turn";
 import TypeTo from "components/story/TypeTo";
 import React from "react";
@@ -11,6 +12,7 @@ const Story = () => {
       <Turn />
       <NameFrom />
       <TypeTo />
+      <NameTo />
     </div>
   );
 };

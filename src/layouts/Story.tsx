1047bca99696ae32878bb204433c03aae41d9ed7
blcklamb <whenucan35@gmail.com>
2023-09-24 00:34:49 +0900

    add: layout component render\ - TODO: apply funnel

--- src/layouts/Story.tsx
+++ src/layouts/Story.tsx
@@ -1,10 +1,16 @@
 import Intro from "components/story/Intro";
+import NameFrom from "components/story/NameFrom";
+import Turn from "components/story/Turn";
+import TypeTo from "components/story/TypeTo";
 import React from "react";
 
 const Story = () => {
   return (
     <div>
       <Intro />
+      <Turn />
+      <NameFrom />
+      <TypeTo />
     </div>
   );
 };

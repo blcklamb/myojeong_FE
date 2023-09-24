a6dd2b86abc0748c0078dda8c4739deaba9e4935
Sangmin Yoon <sangminyn@gmail.com>
2023-09-24 20:34:20 +0900

    add: dropdown 버튼 (#4)
    * style: wish컴포넌트 퍼블리싱
    
    * add: 송편, 공유 이미지 추가
    
    * style:  이미지 변경, 동적 이미지 할당
    
    * style: layout
    
    * add: IconLoader에 share등록
    
    * refactor: 기존 share svg를 아이콘로더 방식으로 변경
    
    * add: navigation svg 추가
    
    * add: dropdown 컴포넌트
    
    * style: dalnara 레이아웃 컴포넌트 조립, dropdown 스타일 변경

---
+++ src/components/dalnara/Dropdown.tsx
@@ -0,0 +1,97 @@
+import Icon from "components/Icon";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { COLORS } from "styles/color";
+
+interface DropdownProps {
+  options: string[]; // 드롭다운 옵션들의 배열
+  onSelect: (selectedOption: string) => void; // 옵션 선택 시 호출할 콜백 함수
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ options, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    onSelect(option);
+  };
+
+  return (
+    <StyledDropdownContainer>
+      <StyledDropdownButton onClick={toggleDropdown}>
+        {selectedOption || (
+          <>
+            <StyledNaviWrapper>
+              <Icon name="navigation" height={20} width={20} />
+              <span>정렬</span>
+            </StyledNaviWrapper>
+          </>
+        )}
+      </StyledDropdownButton>
+      {isOpen && (
+        <StyledDropdownList>
+          {options.map((option, index) => (
+            <StyledDropdownListItem key={index} onClick={() => handleOptionClick(option)}>
+              {option}
+            </StyledDropdownListItem>
+          ))}
+        </StyledDropdownList>
+      )}
+    </StyledDropdownContainer>
+  );
+};
+
+export default Dropdown;
+
+const StyledDropdownContainer = styled.div`
+  width: 130px;
+  height: 48px;
+  position: relative;
+  display: inline-block;
+`;
+
+const StyledDropdownButton = styled.button`
+  width: 100%;
+  height: 100%;
+  border: 0.3rem solid ${COLORS.GREEN};
+  border-radius: 2.5rem;
+  font-size: 24px;
+  background-color: ${COLORS.WHITE};
+  cursor: pointer;
+`;
+
+const StyledDropdownList = styled.ul`
+  width: 100%;
+  margin-top: 5px;
+  border: 0.3rem solid ${COLORS.GREEN};
+  border-radius: 20px;
+  background-color: ${COLORS.WHITE};
+  font-size: 24px;
+  position: absolute;
+  overflow: hidden;
+`;
+
+const StyledDropdownListItem = styled.li`
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${COLORS.LIGHT_GREY};
+  }
+
+  &:not(:last-child) {
+    border-bottom: 2px solid ${COLORS.GREEN};
+  }
+`;
+
+const StyledNaviWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+`;
--- src/layouts/Dalnara.tsx
+++ src/layouts/Dalnara.tsx
@@ -1,10 +1,25 @@
+import Dropdown from "components/dalnara/Dropdown";
 import Wish from "components/dalnara/wishCard/Wish";
+import Input from "components/story/Input";
+
 import React from "react";
 import { styled } from "styled-components";
 
 const Dalnara = () => {
+  const onClick = () => {
+    console.log("dd");
+  };
   return (
     <StyledDalnara>
+      <StyledInputWrapper>
+        <Input value="" height={4.8} placeholder="소원작성자 검색" onChangeInput={onClick} />
+        <Dropdown options={["최신", "좋아요"]} onSelect={onClick} />
+      </StyledInputWrapper>
+      <Wish />
+      <Wish />
+      <Wish />
+      <Wish />
+      <Wish />
       <Wish />
     </StyledDalnara>
   );
@@ -17,5 +32,12 @@ const StyledDalnara = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 10px;
+`;
+
+const StyledInputWrapper = styled.div`
+  margin: 25px 0px;
+  width: 320px;
+  display: flex;
   gap: 10px;
 `;

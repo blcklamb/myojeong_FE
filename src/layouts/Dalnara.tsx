388dd241b2b6b05b4e96d89f1d16b22ecdf41759
Sangmin Yoon <sangminyn@gmail.com>
2023-09-24 15:50:17 +0900

    Publish/dalnara:wish (#3)
    * style: wish컴포넌트 퍼블리싱
    
    * add: 송편, 공유 이미지 추가
    
    * style:  이미지 변경, 동적 이미지 할당
    
    * style: layout

--- src/layouts/Dalnara.tsx
+++ src/layouts/Dalnara.tsx
@@ -1,7 +1,21 @@
+import Wish from "components/dalnara/wishCard/Wish";
 import React from "react";
+import { styled } from "styled-components";
 
 const Dalnara = () => {
-  return <div>Dalnara</div>;
+  return (
+    <StyledDalnara>
+      <Wish />
+    </StyledDalnara>
+  );
 };
 
 export default Dalnara;
+
+const StyledDalnara = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;

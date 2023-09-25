f8583616cec26a04ac36c53178eae614e5e7142c
Sangmin Yoon <sangminyn@gmail.com>
2023-09-26 02:43:47 +0900

    refactor: 송편타입, px단위, dropdown 스타일 변경 (#11)
    * refactor: 송편타입 변경, 필요없는 파일 삭제
    
    * 하드코딩 -> 목데이터 변경
    
    * style: px단위 rem으로 변경
    
    * style: dropdown sticky 적용

--- src/components/dalnara/wishCard/Wish.tsx
+++ src/components/dalnara/wishCard/Wish.tsx
@@ -3,8 +3,16 @@ import SongpyeonBtn from "./SongpyeonBtn";
 import { COLORS } from "styles/color";
 import Icon from "components/Icon";
 
+export interface IWishList {
+  id?: number;
+  from_name: string;
+  content: string;
+  sp1: number;
+  sp2: number;
+  sp3: number;
+}
 
-const Wish = () => {
+const Wish = ({ from_name, content, sp1, sp2, sp3 }: IWishList) => {
   return (
     <>
       <StyledWish>
@@ -12,13 +20,13 @@ const Wish = () => {
           <Icon name="share" width={24} height={24} />
         </StyledHeader>
         <StyledBody>
-          <StyledWishContent>잠재력이 아니라 잠과 재력을 각각 따로 주세요!</StyledWishContent>
-          <StyledWisher>- 테일러</StyledWisher>
+          <StyledWishContent>{content}</StyledWishContent>
+          <StyledWisher>- {from_name}</StyledWisher>
         </StyledBody>
         <StyledFooter>
-          <SongpyeonBtn songpyeonType="love" />
-          <SongpyeonBtn songpyeonType="money" />
-          <SongpyeonBtn songpyeonType="luck" />
+          <SongpyeonBtn songpyeonType="sp10" count={sp1} />
+          <SongpyeonBtn songpyeonType="sp11" count={sp2} />
+          <SongpyeonBtn songpyeonType="sp13" count={sp3} />
         </StyledFooter>
       </StyledWish>
     </>
@@ -28,8 +36,8 @@ const Wish = () => {
 export default Wish;
 
 const StyledWish = styled.div`
-  width: 320px;
-  border-radius: 25px 25px 0px 0px;
+  width: 32rem;
+  border-radius: 2.5rem 2.5rem 0 0;
 
   background-color: ${COLORS.WHITE};
 
@@ -38,16 +46,16 @@ const StyledWish = styled.div`
 `;
 
 const StyledHeader = styled.div`
-  margin-top: 20px;
-  padding: 0px 23px;
+  margin-top: 2rem;
+  padding: 0 2.3rem;
 
   display: flex;
   justify-content: flex-end;
 `;
 
 const StyledBody = styled.div`
-  padding: 0px 23px;
-  font-size: 24px;
+  padding: 0 2.3rem;
+  font-size: 2.4rem;
 `;
 
 const StyledWishContent = styled.div`
@@ -56,12 +64,12 @@ const StyledWishContent = styled.div`
 
 const StyledWisher = styled.h3`
   text-align: right;
-  font-size: 20px;
+  font-size: 2rem;
 `;
 
 const StyledFooter = styled.div`
-  height: 51px;
-  padding: 0px 5px;
+  height: 5.1rem;
+  padding: 0 0.5rem;
   background-color: ${COLORS.LIGHT_GREY};
 
   display: flex;
--- src/layouts/Dalnara.tsx
+++ src/layouts/Dalnara.tsx
@@ -2,25 +2,40 @@ import Dropdown from "components/dalnara/Dropdown";
 import Wish from "components/dalnara/wishCard/Wish";
 import Input from "components/story/Input";
 
-import React from "react";
+import jsonData from "components/dalnara/data.json";
 import { styled } from "styled-components";
+import { useState } from "react";
 
 const Dalnara = () => {
-  const onClick = () => {
-    console.log("dd");
+  const [inputValue, setInputValue] = useState("");
+  const { wish_list } = jsonData;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
   };
+
+  const onClick = (option: string) => {
+    console.log(option);
+  };
+
   return (
     <StyledDalnara>
       <StyledInputWrapper>
-        <Input value="" height={4.8} placeholder="소원작성자 검색" onChangeInput={onClick} />
+        <Input value={inputValue} height={4.8} placeholder="소원작성자 검색" onChangeInput={handleInputChange} />
         <Dropdown options={["최신", "좋아요"]} onSelect={onClick} />
       </StyledInputWrapper>
-      <Wish />
-      <Wish />
-      <Wish />
-      <Wish />
-      <Wish />
-      <Wish />
+      {wish_list.map((wish) => (
+        <Wish key={wish.id} from_name={wish.from_name} content={wish.content} sp1={100} sp2={1000} sp3={90} />
+      ))}
+      {wish_list.map((wish) => (
+        <Wish key={wish.id} from_name={wish.from_name} content={wish.content} sp1={100} sp2={1000} sp3={90} />
+      ))}
+      {wish_list.map((wish) => (
+        <Wish key={wish.id} from_name={wish.from_name} content={wish.content} sp1={100} sp2={1000} sp3={90} />
+      ))}
+      {wish_list.map((wish) => (
+        <Wish key={wish.id} from_name={wish.from_name} content={wish.content} sp1={100} sp2={1000} sp3={90} />
+      ))}
     </StyledDalnara>
   );
 };
@@ -32,12 +47,15 @@ const StyledDalnara = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 10px;
+  gap: 1rem;
 `;
 
 const StyledInputWrapper = styled.div`
-  margin: 25px 0px;
-  width: 320px;
+  margin: 2.5rem 0;
+  width: 32rem;
+  position: sticky;
+  top: 0;
+  background-color: rgba(9, 35, 46, 0.5);
   display: flex;
-  gap: 10px;
+  gap: 1rem;
 `;

60b0856e31bc7e931dd102144af8be932f332948
Chaejung Kim <whenucan35@gmail.com>
2023-09-26 12:09:44 +0900

    add: Publish/start (#12)
    * style: layout for start page
    
    * fix: route path
    - relative to absolute
    
    * fix: refetch error
    - etc. add alert for copy clip board

---
+++ src/components/start/Start.tsx
@@ -0,0 +1,63 @@
+import Button from "components/story/Button";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import { COLORS } from "styles/color";
+
+const Start = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <StyledTop>
+        <StyledH4>소원을 빌면 송편을 나눠줘요</StyledH4>
+        <StyledH1>묘정 송편</StyledH1>
+      </StyledTop>
+      <StyledMiddle>
+        <img src="https://picsum.photos/120/120" alt="myojeong" />
+      </StyledMiddle>
+      <StyledBottom>
+        <Button
+          text="묘정 이야기 듣기"
+          onClick={() => navigate("/story")}
+          type="primary"
+          color="BROWN"
+        ></Button>
+        <Button
+          text="달나라 가기"
+          onClick={() => navigate("/dalnara")}
+          type="primary"
+          color="DARK_BROWN"
+        ></Button>
+      </StyledBottom>
+    </>
+  );
+};
+
+export default Start;
+
+const StyledTop = styled.div`
+  margin-top: 11rem;
+`;
+
+const StyledH4 = styled.h4`
+  color: ${COLORS.WHITE};
+  font-size: 2rem;
+`;
+
+const StyledH1 = styled.h1`
+  color: ${COLORS.WHITE};
+  font-size: 6.4rem;
+`;
+
+const StyledMiddle = styled.div`
+  margin-top: 1.8rem;
+`;
+
+const StyledBottom = styled.div`
+  margin-top: 11rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+`;

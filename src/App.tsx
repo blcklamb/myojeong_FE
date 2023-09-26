30564e3e4186ed4791939dae738ef1d6a065e5ae
blcklamb <whenucan35@gmail.com>
2023-09-26 11:53:25 +0900

    fix: route path - relative to absolute

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,23 @@ import GlobalStyle from "styles/GlobalStyle";
 import { IconLoader } from "components/IconLoader";
 import Songpyeon from "components/songpyeon/Songpyeon";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Start from "components/start/Start";
 
 function App() {
   const queryClient = new QueryClient();
+  // TODO: 소원 작성 데이터 react-query로 저장 후 Songpyeon에 props로 넘기기
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
         <IconLoader />
         <Routes>
-          <Route path="/story" Component={Story} />
-          <Route path="/songpyeon?/:wishId" Component={Songpyeon} />
-          <Route path="/dalnara" Component={Dalnara} />
+          <Route path="story" Component={Story} />
+          <Route path="songpyeon" Component={Songpyeon}>
+            <Route path=":wishId" />
+          </Route>
+          <Route path="dalnara" Component={Dalnara} />
+          <Route path="" Component={Start} />
         </Routes>
       </QueryClientProvider>
     </div>
